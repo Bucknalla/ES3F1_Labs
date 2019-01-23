dd834c24511cdcbfc60c6d18a006093e3eba511b
Alex Bucknall <alex.bucknall@gmail.com>
2019-01-23 15:58:02 +0000

    Added template.c file

---
+++ lab_1/src/template.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xparameters.h"
+#include <stdlib.h>
+#include "xil_printf.h"
+
+void generate_array()
+{
+ 
+};
+
+void multiply_arrays()
+{
+
+};
+
+int main()
+{
+
+  init_platform();
+  xil_printf("Starting Program...");
+  
+  cleanup_platform();
+  return 0;
+};
