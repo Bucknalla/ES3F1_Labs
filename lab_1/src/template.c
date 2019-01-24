38fb3716bfff0bd849c19c777c55f2fc43c0cc98
Alex Bucknall <alex.bucknall@gmail.com>
2019-01-24 14:37:38 +0000

    Adjusted template.c to make lab more clear

--- lab_1/src/template.c
+++ lab_1/src/template.c
@@ -4,21 +4,24 @@
 #include <stdlib.h>
 #include "xil_printf.h"
 
-void generate_array()
+void generate_array(/* ARRAY */)
 {
- 
+  /* Logic to generate array with rand() */
 };
 
-void multiply_arrays()
+void multiply_arrays(/* ARRAY_X, ARRAY_Y, ARRAY_Z*/)
 {
-
+  /* Logic to multiply two arrays together and store in a result array */
 };
 
 int main()
 {
-
   init_platform();
   xil_printf("Starting Program...");
+
+  generate_array(/* ARRAY */);
+
+  multiply_arrays(/* ARRAY_X, ARRAY_Y, ARRAY_Z*/);
   
   cleanup_platform();
   return 0;
